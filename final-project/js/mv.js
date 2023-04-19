21d1c4c075b23a530a9d1baeb7812c1307165f15
winnielincmu <123519891+winnielincmu@users.noreply.github.com>
2023-04-19 01:28:16 -0400

    finished landing and mv pages, move onto home page

--- final-project/js/mv.js
+++ final-project/js/mv.js
@@ -1,6 +1,22 @@
-function buttonsAppear() {
-    const mvButtons = document.querySelector('.buttons');
-    mvButtons.fadeIn(5000);
+var opacity = 0;
+var intervalCount = 0;
+
+setTimeout(startFade, 20000);
+
+function startFade() {
+    setInterval(buttonsAppear, 100);
 }
 
-setTimeout(buttonsAppear, 5000);
+// fade in function adjusted from: https://www.geeksforgeeks.org/how-to-add-fade-in-effect-using-pure-javascript/#
+
+function buttonsAppear() {
+    var mvButtons = document.querySelector('.buttons');
+    var opacity = Number(window.getComputedStyle(mvButtons).getPropertyValue('opacity'));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        mvButtons.style.opacity = opacity;
+    } else {
+        clearInterval(intervalCount);
+    }
+}
+
