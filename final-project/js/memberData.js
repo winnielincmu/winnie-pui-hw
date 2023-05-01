7fd3a2f967ce435c30b78b787b5d9916e93f7b47
winnielincmu <123519891+winnielincmu@users.noreply.github.com>
2023-05-01 18:06:42 -0400

    finished member profile pages, adjusted link click boxes

--- final-project/js/memberData.js
+++ final-project/js/memberData.js
@@ -4,8 +4,12 @@ const members = {
         "emblemBlack": "ahri-emblem-black.png",
         "emblemColored": "ahri-emblem.png",
         "title": "The Queen",
-        "bioTop": "Ahri is the <strong>leader and main vocalist</strong> of K/DA. After being named the most talented new artist in the 2013 Pop Shine Awards, Ahri quickly became a fan favorite with her talented skills and striking looks. She is voiced by (G)I-DLE’s Miyeon.",
-        "bioBottom": "Besides K/DA, Ahri pursues her interests in <strong>beauty and fashion</strong>. She is the current face of FOXY cosmetics, has launched her own fragrance line, and attracts top fashion designers worldwide.",
+        "bioTopBefore": "Ahri is the ",
+        "role": "leader and main vocalist",
+        "bioTopAfter": " of K/DA. After being named the most talented new artist in the 2013 Pop Shine Awards, Ahri quickly became a fan favorite with her talented skills and striking looks. She is voiced by (G)I-DLE’s Miyeon.",
+        "bioBottomBefore": "Besides K/DA, Ahri pursues her interests in ",
+        "strengths": "beauty and fashion",
+        "bioBottomAfter": ". She is the current face of FOXY cosmetics, has launched her own fragrance line, and attracts top fashion designers worldwide.",
         "link": "https://www.leagueoflegends.com/en-us/champions/ahri/"
     },
 
@@ -14,8 +18,12 @@ const members = {
         "emblemBlack": "evelynn-emblem-black.png",
         "emblemColored": "evelynn-emblem.png",
         "title": "The Diva",
-        "bioTop": "Evelynn is the <strong>lead vocalist</strong> of K/DA. Wielding a hypnotic stage presence like no other, Evelynn edges the group towards their most thrilling creative risks. As one of K/DA's co-founders, she is voiced by Madison Beer and Bea Miller.",
-        "bioBottom": "Prior to K/DA, Evelynn has released two hit singles, Agony's Embrace and Ecstasy. As someone who takes her artistry very seriously, Evelynn is well-loved by her diehard fanbase, who claims that she is an <strong>absolute master of the stage</strong>.",
+        "bioTopBefore": "Evelynn is the ",
+        "role": "lead vocalist",
+        "bioTopAfter": " of K/DA. Wielding a hypnotic stage presence like no other, Evelynn edges the group towards their most thrilling creative risks. She is voiced by Madison Beer and Bea Miller.",
+        "bioBottomBefore": "Prior to K/DA, Evelynn has released two hit singles, Agony's Embrace and Ecstasy. As someone who takes her artistry very seriously, Evelynn is well-loved by her diehard fanbase, who claims that she is an ",
+        "strengths": "absolute master",
+        "bioBottomAfter": "of the stage.",
         "link": "https://www.leagueoflegends.com/en-us/champions/evelynn/" 
     },
 
@@ -24,8 +32,12 @@ const members = {
         "emblemBlack": "akali-emblem-black.png",
         "emblemColored": "akali-emblem.png",
         "title": "The Rebel",
-        "bioTop": "Akali is the <strong>main rapper</strong> of K/DA. Despite being the youngest and newest member, Akali's addicting and powerful rap style skyrocketed her fame as an international popstar. She is voiced by (G)I-DLE’s Soyeon.",
-        "bioBottom": "As a creative spirit, Akali has been named a <strong>lyrical genius</strong> in the music industry. Whenever she can, Akali pays homage to her musical roots as a street performer. Her background continues to fuel her daring projects as a source of inspiration.",
+        "bioTopBefore": "Akali is the ",
+        "role": "main rapper",
+        "bioTopAfter": " of K/DA. Despite being the youngest member, Akali's addicting and powerful rap style skyrocketed her fame as an international popstar. She is voiced by (G)I-DLE's Soyeon.",
+        "bioBottomBefore": "As a creative spirit, Akali has been named a ",
+        "strengths": "lyrical genius",
+        "bioBottomAfter": " in the music industry. Whenever she can, Akali pays homage to her musical roots as a street performer. Her background continues to fuel her daring projects as a source of inspiration.",
         "link": "https://www.leagueoflegends.com/en-us/champions/akali/"
     },
 
@@ -34,8 +46,12 @@ const members = {
         "emblemBlack": "kaisa-emblem-black.png",
         "emblemColored": "kaisa-emblem.png",
         "title": "The Dancer",
-        "bioTop": "Kai'sa is the <strong>main dancer and sub rapper</strong> of K/DA. Having won many awards as a choreographer, Kai'sa passion for dance never fails to bring K/DA into the spotlight. She is mainly voiced by Jaira Burns and Wolftyla.",
-        "bioBottom": "Embracing a cosmopolitan approach to life, Kai'sa spends her time traveling around the world to learn about dance. Her perserverance towards perfection and pristine attention to detail makes her an <strong>undeniable and captivating gem</strong> in K/DA.",
+        "bioTopBefore": "Kai'sa is the ",
+        "role": "main dancer and sub rapper",
+        "bioTopAfter": " of K/DA. Having won many awards as a choreographer, Kai'sa passion for dance never fails to bring K/DA into the spotlight. She is voiced by Jaira Burns and Wolftyla.",
+        "bioBottomBefore": "Embracing a cosmopolitan approach to life, Kai'sa spends her time traveling around the world to learn about dance. Her perserverance towards perfection and pristine attention to detail makes her an",
+        "strengths": "undeniable and captivating gem",
+        "bioBottomAfter": " in K/DA.",
         "link": "https://www.leagueoflegends.com/en-us/champions/kai-sa/"
     }
 }
--- final-project/js/profile.js
+++ final-project/js/profile.js
@@ -12,7 +12,7 @@ let memberTitle = document.querySelector('#member-title');
 let memberBioTop = document.querySelector('#top-bio');
 let memberBioBottom = document.querySelector('#bottom-bio');
 let memberEmblem = document.querySelector('.member-emblems');
-let memberLink = document.querySelector('.member-link');
+let memberLink = document.querySelector('#external-details');
 let memberButtonCaption = document.querySelector('.more-button span');
 
 // assign respective content to the members' profiles
@@ -20,27 +20,72 @@ let memberButtonCaption = document.querySelector('.more-button span');
 memberImage.src = './assets/cutouts-and-logo/' + members[memberIdentity]["profilePic"];
 memberName.innerText = memberIdentity;
 memberTitle.innerText = members[memberIdentity]["title"];
-memberBioTop.innerText = members[memberIdentity]["bioTop"];
-memberBioBottom.innerText = members[memberIdentity]["bioBottom"];
 memberEmblem.src = './assets/cutouts-and-logo/' + members[memberIdentity]["emblemBlack"];
 memberLink.href = members[memberIdentity]["link"];
 memberButtonCaption.innerText = 'More On ' + memberIdentity;
 
-// update size of 'more information' button to fit length of evelynn's name
+// retrieve parts of member description
 
-let moreButton = document.querySelector('.more-button');
+let topTextBefore = document.getElementById('top-before');
+let role = document.getElementById('role');
+let topTextAfter = document.getElementById('top-after');
+let bottomTextBefore = document.getElementById('bottom-before');
+let strengths = document.getElementById('strengths');
+let bottomTextAfter = document.getElementById('bottom-after');
+
+//  update parts of description separately to render correct information on DOM
+
+topTextBefore.innerText = members[memberIdentity]["bioTopBefore"];
+role.innerText = members[memberIdentity]["role"];
+topTextAfter.innerText = members[memberIdentity]["bioTopAfter"];
+bottomTextBefore.innerText = members[memberIdentity]["bioBottomBefore"];
+strengths.innerText = members[memberIdentity]["strengths"];
+bottomTextAfter.innerText = members[memberIdentity]["bioBottomAfter"];
+
+// adjust image placement for three members
 
 if (memberIdentity === "Evelynn") {
-    moreButton.style.width = "75%";
-    memberLink.style.width = "75%";
     memberImage.style.right = 40 + "px";
     memberImage.style.bottom = 40 + "px";
 };
 
 if (memberIdentity === "Kai'sa") {
     memberImage.style.bottom = 40 + "px";
-}
+};
 
 if (memberIdentity === "Akali") {
     memberImage.style.right = 40 + "px";
+};
+
+// create array with all members' colored emblems, will be used to replace duplicated emblem on buttons
+
+let allEmblems = new Array();
+
+for (let member in members) {
+        allEmblems.push(members[member]["emblemColored"]);
+};
+
+// remove emblems that are currently shown on page to identify which one is missing
+
+let shownEmblems = document.getElementsByClassName('member-button');
+
+for (let i = 0; i < shownEmblems.length; i++) {
+
+    let imageName = getImageName(shownEmblems[i].firstElementChild.src);
+    allEmblems.pop(imageName);
+
+    // find position of emblem button that needs to be replaced, update emblem displayed
+
+    if (imageName === members[memberIdentity]["emblemColored"]) {
+        let replacePosition = shownEmblems[i].firstElementChild;
+        replacePosition.src = './assets/cutouts-and-logo/' + String(allEmblems[0]);
+    };
+
+};
+
+// function to extract image name taken from: https://stackoverflow.com/questions/29182283/javascript-onclick-get-image-name-without-path
+
+function getImageName(fullPath) {
+    let imageName = fullPath.replace(/^.*[\\\/]/, '');
+    return imageName;
 }
